4275279456ec828e80a68e023f0c8e1d3df3a79c
a sullon m <asullom@gmail.com>
2020-09-21 08:35:48 -0500

    Menu

--- Main.java
+++ Main.java
@@ -6,7 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String name= input.nextLine();
-        System.out.println("Escribiste: "+name);
+        //System.out.print("Ingrese nombre:");
+        //String name= input.nextLine();
+        //System.out.println("Escribiste: "+name);
+        int opt = 0;
+        do{
+            System.out.println("***** CRUD PERSON *****");
+            System.out.println("1 List ");
+            System.out.println("2 New ");
+            System.out.println("3 Delete ");
+            System.out.println("0 Exit ");
+            System.out.println("Choice option: ");
+            opt = input.nextInt();
+            System.out.println("You chosed: " + opt);
+            input.nextLine(); // Limpiar el buffer
+            switch (opt) { 
+                case 1: System.out.println("Opcion 1 elejida");
+                
+                break;
+                case 2: System.out.println("Opcion 2 elejida");
+                
+                break;
+                
+                default: System.out.println("Opcion no valida");
+            }
+        }while(opt != 0);
     }
 }
